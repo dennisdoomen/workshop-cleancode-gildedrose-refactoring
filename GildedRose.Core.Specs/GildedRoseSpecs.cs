f8f754933de81dbc15b0187722cea826bba6c362
Dennis Doomen <dennis.doomen@avivasolutions.nl>
2021-06-17 20:33:45 +0200

    Better characteristics tests

--- GildedRose.Core.Specs/GildedRoseSpecs.cs
+++ GildedRose.Core.Specs/GildedRoseSpecs.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using FluentAssertions;
 using Xunit;
 
@@ -8,145 +6,104 @@ namespace GildedRose.Core.Specs
 {
     public class GildedRoseSpecs
     {
-        private const int MaxBackstageSellin = 30;
-        private const int MaxQuality = 50;
-        private GildedRose gildedRose;
-        private List<Item> items;
-        private Random rand = new Random(3456789);
-
-        public GildedRoseSpecs()
+        [Theory]
+        [InlineData("+5 Dexterity Vest", 10, 20, 9, 19)]
+        [InlineData("Aged Brie", 2, 0, 1, 1)]
+        [InlineData("Elixir of the Mongoose", 5, 7, 4, 6)]
+        [InlineData("Sulfuras, Hand of Ragnaros", 0, 80, 0, 80)]
+        [InlineData("Backstage passes to a TAFKAL80ETC concert", 15, 20, 14, 21)]
+        [InlineData("Conjured Mana Cake", 3, 6, 2, 5)]
+        public void After_one_day(string name, int startingSellIn, int startingQuality, int expectedSellIn, int expectedQuality)
         {
-            gildedRose = new GildedRose();
-            items = gildedRose.MakeItems();
+            // Arrange
+            GildedRose gildedRose = new();
+
+            Item item = new(name, startingSellIn, startingQuality);
+            
+            // Act
+            gildedRose.UpdateQuality(new List<Item> {item});
+
+            // Assert
+            item.SellIn.Should().Be(expectedSellIn);
+            item.Quality.Should().Be(expectedQuality);
         }
 
-        [Fact]
-        public void After_one_day()
+        [Theory]
+        [InlineData("+5 Dexterity Vest", 10, 20, 7, 17)]
+        [InlineData("Aged Brie", 2, 0, -1, 4)]
+        [InlineData("Elixir of the Mongoose", 5, 7, 2, 4)]
+        [InlineData("Sulfuras, Hand of Ragnaros", 0, 80, 0, 80)]
+        [InlineData("Backstage passes to a TAFKAL80ETC concert", 15, 20, 12, 23)]
+        [InlineData("Conjured Mana Cake", 3, 6, 0, 3)]
+        public void After_three_days(string name, int startingSellIn, int startingQuality, int expectedSellIn, int expectedQuality)
         {
-            RepeatUpdateQuality(1);
-            var itemNames = items.Select(i => i.Name);
-            
-            itemNames.Should().BeEquivalentTo(new[]
-            {
-                "+5 Dexterity Vest",
-                "Aged Brie",
-                "Elixir of the Mongoose",
-                "Sulfuras, Hand of Ragnaros",
-                "Backstage passes to a TAFKAL80ETC concert",
-                "Conjured Mana Cake"
-            });
-            
-            var qualities = items.Select(i => i.Quality);
-            qualities.Should().BeEquivalentTo(new[] { 19, 1, 6, 80, 21, 5 });
-            
-            var sellIns = items.Select(i => i.SellIn);
-            sellIns.Should().BeEquivalentTo(new[] { 9, 1, 4, 0, 14, 2 });
-        }
+            // Arrange
+            GildedRose gildedRose = new();
 
-        [Fact]
-        public void After_three_days()
-        {
-            RepeatUpdateQuality(3);
+            Item item = new(name, startingSellIn, startingQuality);
             
-            var itemNames = items.Select(i => i.Name);
-            itemNames.Should().BeEquivalentTo(new[]
+            // Act
+            for (int day = 0; day < 3; day++)
             {
-                "+5 Dexterity Vest",
-                "Aged Brie",
-                "Elixir of the Mongoose",
-                "Sulfuras, Hand of Ragnaros",
-                "Backstage passes to a TAFKAL80ETC concert",
-                "Conjured Mana Cake"
-            });
-            
-            var qualities = items.Select(i => i.Quality);
-            qualities.Should().BeEquivalentTo(new[] { 17, 4, 4, 80, 23, 3 });
-            
-            var sellIns = items.Select(i => i.SellIn);
-            sellIns.Should().BeEquivalentTo(new[] { 7, -1, 2, 0, 12, 0 });
-        }
-
-        [Fact]
-        public void After_a_shitload_of_days()
-        {
-            RepeatUpdateQuality(500);
-
-            var itemNames = items.Select(i => i.Name);
-            itemNames.Should().BeEquivalentTo(new string[]
-            {
-                "+5 Dexterity Vest",
-                "Aged Brie",
-                "Elixir of the Mongoose",
-                "Sulfuras, Hand of Ragnaros",
-                "Backstage passes to a TAFKAL80ETC concert",
-                "Conjured Mana Cake"
-            });
-            
-            var qualities = items.Select(i => i.Quality);
-            qualities.Should().BeEquivalentTo(new int[] { 0, 50, 0, 80, 0, 0 });
-            
-            var sellIns = items.Select(i => i.SellIn);
-            sellIns.Should().BeEquivalentTo(new int[] { -490, -498, -495, 0, -485, -497 });
-        }
-
-        [Fact]
-        public void Backstage_pass_golden_copy()
-        {
-            items = ABunchOfBackstagePasses();
-            RepeatUpdateQuality(11);
-            
-            var qualities = items.Select(i => i.Quality);
-            qualities.Should().BeEquivalentTo(new int[]
-            {
-                0, 49, 18, 33, 0, 28, 0, 12, 34, 25, 0, 0, 50, 12, 0, 0, 11, 0, 50, 28, 0, 0, 0, 0, 45, 0, 24, 50, 31, 50, 50, 0, 0, 0, 45, 29, 
-                50, 0, 0, 47, 49, 50, 27, 26, 50, 0, 27, 36, 0, 27, 46, 33, 50, 0, 50, 0, 31, 0, 0, 50, 0, 15, 0, 47, 30, 0, 0, 0, 0, 15, 0, 0, 
-                36, 50, 45, 50, 37, 43, 48, 50, 13, 0, 0, 50, 39, 47, 0, 24, 30, 42, 22, 50, 0, 50, 0, 38, 41, 50, 36, 43
-            });
-            
-            var sellIns = items.Select(i => i.SellIn);
-            sellIns.Should().BeEquivalentTo(new int[]
-            {
-                -9, 10, 15, 6, -11, 6, -7, 18, 14, 15, -10, -11, 3, 9, -4, -4, 14, -7, 18, 0, -8, -1, -4, -7, 14, -9, 2, 11, 11, 8, 10, -4, -11, 
-                -10, 15, 3, 15, -8, -2, 4, 14, 11, 8, 5, 10, -10, 0, 10, -3, 1, 8, 13, 3, -7, 11, -8, 1, -11, -5, 7, -11, 18, -7, 0, 12, -5, -6, 
-                -8, -6, 12, -10, -6, 7, 7, 3, 15, 2, 8, 3, 4, 16, -7, -11, 18, 11, 5, -6, 11, 16, 5, 13, 15, -8, 11, -5, 16, 6, 2, 2, 2
-            });
-        }
-
-        private void RepeatUpdateQuality(int times)
-        {
-            for (int i = 0; i < times; i++)
-            {
-                gildedRose.UpdateQuality(items);
-            }
-        }
-
-        private List<Item> ABunchOfBackstagePasses()
-        {
-            var listOfPasses = new List<Item>();
-            for (int i = 0; i < 100; i++)
-            {
-                listOfPasses.Add(ARandomBackstagePass());
+                gildedRose.UpdateQuality(new List<Item> {item});
             }
 
-            return listOfPasses;
+            // Assert
+            item.SellIn.Should().Be(expectedSellIn);
+            item.Quality.Should().Be(expectedQuality);
         }
 
-        private int RandomSellIn()
+        [Theory]
+        [InlineData("+5 Dexterity Vest", 10, 20, 0, 10)]
+        [InlineData("Aged Brie", 2, 0, -8, 18)]
+        [InlineData("Elixir of the Mongoose", 5, 7, -5, 0)]
+        [InlineData("Sulfuras, Hand of Ragnaros", 0, 80, 0, 80)]
+        [InlineData("Backstage passes to a TAFKAL80ETC concert", 15, 20, 5, 35)]
+        [InlineData("Conjured Mana Cake", 3, 6, -7, 0)]
+        public void After_ten_days(string name, int startingSellIn, int startingQuality, int expectedSellIn, int expectedQuality)
         {
-            return rand.Next(MaxBackstageSellin);
-        }
+            // Arrange
+            GildedRose gildedRose = new();
 
-        private int RandomQuality()
-        {
-            return rand.Next(MaxQuality);
-        }
-
-        private Item ARandomBackstagePass()
-        {
-            int quality = RandomQuality();
-            int sellIn = RandomSellIn();
+            Item item = new(name, startingSellIn, startingQuality);
             
-            return new Item("Backstage passes to a TAFKAL80ETC concert", sellIn, quality);
+            // Act
+            for (int day = 0; day < 10; day++)
+            {
+                gildedRose.UpdateQuality(new List<Item> {item});
+            }
+
+            // Assert
+            item.SellIn.Should().Be(expectedSellIn);
+            item.Quality.Should().Be(expectedQuality);
+        }
+
+        [Theory]
+        [InlineData(1, 11)]
+        [InlineData(5, 15)]
+        [InlineData(10, 20)]
+        [InlineData(11, 22)]
+        [InlineData(15, 30)]
+        [InlineData(20, 45)]
+        [InlineData(21, 0)]
+        public void Backstage_passes_increase_in_quality_quickly_until_they_are_due(int daysPassed, int expectedQuality)
+        {
+            // Arrange
+            GildedRose gildedRose = new();
+
+            int nrDaysUntilConcert = 20;
+
+            Item item = new("Backstage passes to a TAFKAL80ETC concert", sellIn: nrDaysUntilConcert, quality: 10);
+            
+            // Act
+            for (int day = 0; day < daysPassed; day++)
+            {
+                gildedRose.UpdateQuality(new List<Item> {item});
+            }
+
+            // Assert
+            item.SellIn.Should().Be(nrDaysUntilConcert - daysPassed);
+            item.Quality.Should().Be(expectedQuality);
         }
     }
 }
